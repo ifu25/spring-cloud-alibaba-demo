41594129378d7e61199144b3f04ef22a98c8ee0f
xinggang <xinggang.china@gmail.com>
2022-03-06 16:40:57 +0800

    🏷️完成：服务注册

--- lttc-gateway/src/main/java/cn/lttc/lttcgateway/GatewayApplication.java
+++ lttc-gateway/src/main/java/cn/lttc/lttcgateway/GatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LttcGatewayApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LttcGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
--- lttc-system/src/main/java/com/example/lttcsystem/SystemApplication.java
+++ lttc-system/src/main/java/com/example/lttcsystem/SystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LttcSystemApplication {
+public class SystemApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LttcSystemApplication.class, args);
+        SpringApplication.run(SystemApplication.class, args);
     }
 
 }
